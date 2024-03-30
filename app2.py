9c9c46a88483e0dec22522993f3ca7fe1d61ddfe
anisen10 <163909909+anisen10@users.noreply.github.com>
2024-03-29 21:52:25 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,18 +12,12 @@ from sklearn.linear_model import LogisticRegression
 import streamlit as st
 import joblib
 
-# Set page configuration
 st.set_page_config(page_title="Twitter Sentiment Analysis", layout="wide")
-# Load trained logistic regression model
 lr_model = joblib.load('lr_model.pkl')
 
-# Load stopwords
 stop_words = set(stopwords.words('english'))
-
-# Initialize WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
-# Load and preprocess the training data
 column_names = ['Target', 'ID', 'Date', 'Flag', 'User', 'Text']
 total_rows = sum(1 for line in open('training.1600000.processed.noemoticon.csv', encoding='latin1'))
 nrows = 50000
@@ -36,14 +30,12 @@ df['Text'] = df['Text'].apply(lambda x: [word for word in x if word not in stop_
 df['Text'] = df['Text'].apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
 df['Text'] = df['Text'].apply(lambda x: ' '.join(x).lower())
 
-# Initialize and fit the TfidfVectorizer
 tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 X = tfidf_vectorizer.fit_transform(df['Text'])
 y = df['Target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 lr_model.fit(X_train, y_train)
 
-# Set background image
 st.markdown(
     """
     <style>
@@ -72,46 +64,26 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Function to preprocess a single sentence
 def preprocess_sentence(sentence):
-    # Lowercase the sentence
     sentence = sentence.lower()
-    # Remove punctuation
     sentence = re.sub(r'[^\w\s]', '', sentence)
-    # Tokenize the sentence
     tokens = word_tokenize(sentence)
-    # Remove stopwords
     tokens = [word for word in tokens if word not in stop_words]
-    # Lemmatize words
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    # Join tokens into a single string
     processed_sentence = ' '.join(tokens)
     return processed_sentence
 
-# Streamlit app
 def main():
  
-    # Main title
     st.title("Twitter Sentiment Analysis")
-
-    # Input box for user to enter a sentence
     sentence = st.text_input("Enter a sentence:")
 
-    # Button to classify the sentence
     if st.button("Classify"):
-        # Preprocess the sentence
+  
         processed_sentence = preprocess_sentence(sentence)
-
-        # Vectorize the preprocessed sentence using the same TfidfVectorizer
         X_test = tfidf_vectorizer.transform([processed_sentence])
-
-        # Predict sentiment using logistic regression model
         lr_prediction = lr_model.predict(X_test)[0]
-
-        # Map prediction to sentiment label
         sentiment_label = {0: 'Negative', 4: 'Positive'}
-
-        # Display classification result
         st.write(f"The sentiment of the sentence '{sentence}' is: {sentiment_label[lr_prediction]}")
 
 if __name__ == "__main__":
